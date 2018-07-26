6166ae2b82d0b7402832a7511c050d7ae1bde23c
Andchen <1476204794@qq.com>
2018-07-26 21:48:33 +0800

    使用 Selenium 爬取淘宝商品

---
+++ spider.py
@@ -0,0 +1,96 @@
+import pymongo
+from pymongo import MongoClient
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+from pyquery import PyQuery as pq
+from config import *
+from urllib.parse import quote
+
+
+# 无界面模式
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+browser = webdriver.Chrome('/home/feng/chromedriver_linux64/chromedriver',chrome_options=chrome_options)
+
+wait = WebDriverWait(browser, 10)
+client = MongoClient(MONGO_URL)
+db = client[MONGO_DB]
+
+
+def index_page(page):
+    """
+    抓取索引页
+    :param page: 页码
+    """
+    print('正在爬取第', page, '页')
+    try:
+        url = 'https://s.taobao.com/search?q=' + quote(KEYWORD)
+        browser.get(url)
+        if page > 1:
+            input = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#mainsrp-pager div.form > input')))
+            submit = wait.until(
+                EC.element_to_be_clickable((By.CSS_SELECTOR, '#mainsrp-pager div.form > span.btn.J_Submit')))
+            input.clear()
+            # 翻页
+            input.send_keys(page)
+            submit.click()
+        # 等待页面的加载
+        wait.until(
+            EC.text_to_be_present_in_element((By.CSS_SELECTOR, '#mainsrp-pager li.item.active > span'), str(page)))
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '.m-itemlist .items .item')))
+        # 加载成功，执行 get_products()
+        get_products()
+    # 抛出超时异常
+    except TimeoutException:
+        index_page(page)
+
+
+def get_products():
+    """
+    提取商品数据
+    """
+    # 获取源代码
+    html = browser.page_source
+    # 构造了PyQuery解析对象
+    doc = pq(html)
+    items = doc('#mainsrp-itemlist .items .item').items()
+    for item in items:
+        product = {
+            'image': item.find('.pic .img').attr('data-src'),
+            'price': item.find('.price').text(),
+            'deal': item.find('.deal-cnt').text(),
+            'title': item.find('title').text(),
+            'shop': item.find('.shop').text(),
+            'location': item.find('.location').text()
+        }
+
+        print(product)
+        save_to_mongo(product)
+
+
+def save_to_mongo(result):
+    """
+    保存至MongoDB
+    :param result:结果
+    """
+    try:
+        if db[MONGO_COLLECTION].insert(result):
+            print('存储到 MongoDB 成功')
+    except Exception:
+        print('存储到 MongoDB 失败')
+
+
+def main():
+    """
+    遍历每一页
+    """
+    for i in range(1, MAX_PAGE + 1):
+        index_page(i)
+    browser.close()
+
+
+if __name__ == '__main__':
+    main()
