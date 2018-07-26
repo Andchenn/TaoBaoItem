8bc85bee8f56c9b24106a3832ecc9f4d14c015f8
Andchen <1476204794@qq.com>
2018-07-26 21:51:27 +0800

    配置文件

---
+++ config.py
@@ -0,0 +1,9 @@
+MONGO_URL = "mongodb://feng:a@127.0.0.1:27017/?authMechanism=SCRAM-SHA-1"
+MONGO_DB = 'taobao'
+MONGO_COLLECTION = 'products'
+
+KEYWORD = 'iPad'
+
+MAX_PAGE = 3
+
+# SERVICE_ARGS = ['--load-images=false', '--disk-cache=true']
